ceaf3e01dd1dd71921386304299325f953e977fc
Ilya-S-Shutov <ilya.s.shutov@gmail.com>
2025-05-20 02:26:20 +0500

    Доработан класс страницы /profile. Добавлен метод add_cookie_abtest_a() и "костыль" для обхода обновлённого интерфейса.

--- pages/profile_page.py
+++ pages/profile_page.py
@@ -1,22 +1,22 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.remote.webdriver import WebDriver
-from selenium.webdriver.remote.webelement import WebElement
 
 from base_page import BasePage
 
 
 class ProfilePage(BasePage):
-    SEARCH_FIELD_LOCATOR = ''
+    # SEARCH_FIELD_LOCATOR = (By.CLASS_NAME, '')
 
     def __init__(self, driver: WebDriver):
         super().__init__(driver)
         self.URL = f'{self.BASE_URL}/profile'
 
-    def update_cookie(self):
-        pass
+    def add_cookie_abtest_a(self) -> None:
+        self.add_cookie('abtest-profile', 'A')
+        self.refresh()
 
-    def use_search(self, query: str) -> None:
-        pass
+    def skip_stage(self) -> None:
+        self.driver.get('https://saby.ru/profile/page/company-list')
+
+
+    # def use_search(self, query: str) -> None:
+    #     pass
